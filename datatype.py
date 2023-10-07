ded617deb0f1a28375963319f4ee35cd4b5a141b
Gaurav More <gauravmore0010@gmail.com>
2023-10-07 20:38:25 +0530

    "Basic Start"

---
+++ datatype.py
@@ -0,0 +1,7 @@
+var_1 = 123
+var_2 = 10.2
+print(var_1)
+print(var_2)
+print(var_1 + var_2)
+print(type(var_1))
+print(type(var_2))
---
+++ variable.py
@@ -0,0 +1,7 @@
+name=input("What is ur name?")
+print(name)
+
+name="Gaurav"
+length=len(name)
+print(name)
+print(length)
