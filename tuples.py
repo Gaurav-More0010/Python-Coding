e6b7716cadc418fd6afd3e6bdb26196bc3113b18
Gaurav More <gauravmore0010@gmail.com>
2023-10-09 12:50:51 +0530

    "Tuple"

---
+++ tuples.py
@@ -0,0 +1,20 @@
+#in list we used []
+#in tuple we used ()
+#tuple1=(10,2,4,-1)
+#tuple2=("Sharvil,"Gaurav","More")
+#tuple3 =(1,"Krushna",True, 1.2)
+tuple1=(12,6,-8,'Gaurav', True)
+tuple4=(-1,-9,0,4,6,7,9)
+# print(tuple1[1])
+# print(tuple1[-1])
+tuple2=(15,)
+# print(tuple2)
+# tuple3 = (tuple1 + tuple2)
+# print(tuple3)
+print(min(tuple4))
+
+#Convert list to tuple
+list1=[21,5,7,8]
+print(tuple(list1))
+
+#look for tuple methods
