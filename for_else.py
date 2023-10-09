847df20f80a1da40ff6c87bc8e156eb5d67ed0eb
Gaurav More <gauravmore0010@gmail.com>
2023-10-09 14:58:28 +0530

    "For loop"

---
+++ for_else.py
@@ -0,0 +1,7 @@
+tuple1 = (2,4,6,-1,5)
+for i in tuple1:
+    print(i)
+    if(i==6):
+        break
+else:
+    print("Loop succesfully completed")
---
+++ for_loop.py
@@ -0,0 +1,12 @@
+names = ['Gaurav','More']
+for i in names:
+    print(i)
+    if i=='Gaurav':
+        print("Hey, It me")
+
+list1 = [2,5,-7,8]
+squares = []
+for i in list1:
+    square = i ** 2
+    squares.append(square)
+print(squares)
