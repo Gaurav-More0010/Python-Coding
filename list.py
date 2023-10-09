c469c73d0d2e466d11d83cff4edc7271dc445a31
Gaurav More <gauravmore0010@gmail.com>
2023-10-09 10:16:01 +0530

    "list"

---
+++ list.py
@@ -0,0 +1,27 @@
+number = [10,0,-1,9,6]
+# names = ['Gaurav','More',1.2]
+# mix_list = [1,'Bunty',True,10.11]
+# print(number)
+# # print(names)
+# # print(mix_list)
+# print(number[1])
+# print(number[-1])
+# print(number[0:4]) #slicing
+# print(number[0:5:2])
+# number.sort()
+# print(number)
+# number.reverse()
+# print(number)
+# print(max(number))
+# number.append(12)
+# print(number)
+
+number.insert(2,15)
+print(number)
+number.extend([19,21,24,27])
+print(number)
+
+
+
+
+
