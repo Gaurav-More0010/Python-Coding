56ebbdd2bc543fb410675e9702249c2fcd3b31e6
Gaurav More <gauravmore0010@gmail.com>
2023-10-09 14:45:52 +0530

    "Set"

---
+++ Set.py
@@ -0,0 +1,12 @@
+#Dupliates Items are not allowed in sets
+#unorderdoutput
+#indexng,slicing are not allowed in Set
+
+set1 = {2,5,7,8,9,0,'Gaurav', True}
+print(set1)
+set1.add(99)
+print(set1)
+#Check another operaions and methods od set
+
+
+ 
